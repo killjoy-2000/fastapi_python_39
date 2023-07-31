14b877a9fda823345af3f0c2899fdd6011269591
fade <fade@valomail.com>
2023-07-31 14:59:01 +0530

    store mac

--- func/getMac.py
+++ func/getMac.py
@@ -3,9 +3,22 @@
 import uuid
 import sys
 import hashlib
+import sys
+sys.path.append('/home/aritra/KaBooM/deja VU/python-test/fastapi-test-p39-v2/')
 
 def get_mac():
     mac = str(uuid.getnode())
     hash = hashlib.sha256(mac.encode()).hexdigest()
     hash = hash[:6]
+    # fold_id = open(".sec/fold_id.aspic", "r", encoding='utf-8')
+    # print(fold_id.read())
+    with open(".sec/fold_id.aspic", "w", encoding='utf-8') as id:
+        id.write(hash)
     return hash
+
+# get_mac()
+
+def send_mac():
+    with open(".sec/fold_id.aspic", "r", encoding='utf-8') as id:
+        fold_id = id.read()
+    return fold_id
--- routes/login.py
+++ routes/login.py
@@ -9,6 +9,6 @@ router = APIRouter()
 @router.get("/login")
 async def login():
     # id = getMac.get_mac
-    id = getMac.get_mac()
-    print("id >>>>>> ", id)
+    id = getMac.send_mac()
+    # print("id >>>>>> ", id)
     return {"id" : id}
