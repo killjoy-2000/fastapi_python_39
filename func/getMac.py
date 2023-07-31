ad917a68305259beb09445b5913f3a67759d789e
fade <fade@valomail.com>
2023-07-31 12:43:32 +0530

    2st commit

--- func/getMac.py
+++ func/getMac.py
@@ -1,9 +1,11 @@
 # this function will get device mac id
 
 import uuid
+import sys
+import hashlib
 
-def send_mac():
-    mac = uuid.getnode()
-    return mac
-
-print("mac id is >>>>> ", send_mac())
+def get_mac():
+    mac = str(uuid.getnode())
+    hash = hashlib.sha256(mac.encode()).hexdigest()
+    hash = hash[:6]
+    return hash
--- routes/login.py
+++ routes/login.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
-
-
+# add the parent folder path
+import sys
+sys.path.append('/home/aritra/KaBooM/deja VU/python-test/fastapi-test-p39-v2/')
+from func import getMac
 router = APIRouter()
 
 
 @router.get("/login")
-async def login():    
-    return ("login")
+async def login():
+    # id = getMac.get_mac
+    id = getMac.get_mac()
+    print("id >>>>>> ", id)
+    return {"id" : id}
