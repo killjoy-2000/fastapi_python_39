f367d7575252c0ba8017ea913978b99c646988e3
fade <fade@valomail.com>
2023-07-31 12:58:02 +0530

    minor change

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 app.include_router(login.router)
 
-
+# this will take effect if we use python run instead of uvicorn
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
