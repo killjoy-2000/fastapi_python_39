7a2726515bf7a832c217c8a7bd297118587105b3
fade <fade@valomail.com>
2023-08-17 19:59:43 +0530

    add user modified

--- func/user.py
+++ func/user.py
@@ -14,38 +14,64 @@ sheet = wb["Sheet1"]
 print(sheet.title)
 print(sheet["A1"].value)
 
+def add_user(data):
+    try:
+        search_val = data.user_id
+        found_cells = []
+        for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=3):
+            column_b_val = row[1].value
+            if column_b_val == search_val:
+                return "user_exist"
+            # elif column_a_val != search_val:
+        data_to_put = {
+            "A" : data.user,
+            "B" : data.user_id,
+            "C" : data.image_no,
+            "D" : data.image_consumed,
+            "E" : data.tag,
+        }
+        sheet.append(data_to_put)
+        wb.save("test_xl_py.xlsx")
+        return True
+    except Exception as e:
+        print(str(e))
+        return "error >>>>>>>>>> " + str(e)
+
+    # finally:
+    #     wb.close()
+
 # for row in sheet.iter_rows(values_only=True):
 #     for cell in row:
 #         print(cell)
 
-data_to_put = {
-    "A" : "Rudra",
-    "B" : 105,
-    "C" : 200,
-    "D" : 34,
-    "E" : "F",
-}
+# data_to_put = {
+#     "A" : "Rudra",
+#     "B" : 105,
+#     "C" : 200,
+#     "D" : 34,
+#     "E" : "F",
+# }
 
-search_value = "Anwesa"
-found_cells = []
-for row in sheet.iter_rows(values_only=True):
-    for cell in row:
-        if cell == search_value:
-            found_cells.append(row)
+# search_value = "Anwesa"
+# found_cells = []
+# for row in sheet.iter_rows(values_only=True):
+#     for cell in row:
+#         if cell == search_value:
+#             found_cells.append(row)
 
-            # cell_number = f"{get_column_letter(cell.column)}{cell.row}"
-            # print(cell_number)
+#             # cell_number = f"{get_column_letter(cell.column)}{cell.row}"
+#             # print(cell_number)
 
-# Print the found cells
-for row in found_cells:
-    print(row)
-    print(row[3])
-# for cell in found_cells:
-#     print(cell)
-#     print(found_cells)
+# # Print the found cells
+# for row in found_cells:
+#     print(row)
+#     print(row[3])
+# # for cell in found_cells:
+# #     print(cell)
+# #     print(found_cells)
 
-# sheet.append(data_to_put)
-# wb.save("test_xl_py.xlsx")
+# # sheet.append(data_to_put)
+# # wb.save("test_xl_py.xlsx")
 
 
-wb.close()
+# wb.close()
--- routes/store_user.py
+++ routes/store_user.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 
 import sys
 sys.path.append('/home/aritra/KaBooM/deja VU/python-test/fastapi-test-p39-v2/')
-from func import auth
+from func import auth,user
 
 router = APIRouter()
 
@@ -23,34 +23,60 @@ class body(BaseModel):
 
 @router.post("/add_data")
 async def add_data(request: Request, signature: signature, body: body):
-    # print(token)
+    try:
+        # print(token)
 
-    # variable
-    res = {
-        "respstatus":{
-            "rcode" : "",
-            "rmsg" : ""
-        },
-        "respdata" : {        
+        # variable
+        res = {
+            "respstatus":{
+                "rcode" : "",
+                "rmsg" : ""
+            },
+            "respdata" : {        
+            }
         }
-    }
-    # val = auth.validate_token(token)
-    # if(val["status"] == True):
-    #     print(val["body"])
-    #     # print(val)
-    #     return val
-    # if(val["status"] == False):
-    #     return {
-    #         "sts": "fk"
-    #     }
-    if(signature.fid != "F-AU001"):
-        res["respstatus"]["rcode"] = "FAU001"
-        res["respstatus"]["rmsg"] = "Not a valid call"
+        # val = auth.validate_token(token)
+        # if(val["status"] == True):
+        #     print(val["body"])
+        #     # print(val)
+        #     return val
+        # if(val["status"] == False):
+        #     return {
+        #         "sts": "fk"
+        #     }
+        if(signature.fid != "F-AU001"):
+            res["respstatus"]["rcode"] = "FAU001"
+            res["respstatus"]["rmsg"] = "Not a valid call"
+            return res
+        # print(body)
+        # return res
+        elif signature.fid == "F-AU001":
+            token = request.headers.get("token")
+            ip = request.client.host
+            val = auth.validate_token(token, ip)
+
+            if(val["status"] == False):
+                res["respstatus"]["rcode"] = "FUP002"
+                res["respstatus"]["rmsg"] = "failed to validate token"
+                return res
+            
+            elif(val["status"] == True):     
+                print(body)
+                sts = user.add_user(body)
+                print(sts)
+                if sts == "user_exist":
+                    res["respstatus"]["rcode"] = "FUP003"
+                    res["respstatus"]["rmsg"] = "user already exists"
+                    return res
+                elif sts:
+                    res["respstatus"]["rcode"] = "000"
+                    res["respstatus"]["rmsg"] = "User added successfully"
+                    return res
+    except Exception as e:
+        print(str(e))
+        res["respstatus"]["rcode"] = "600"
+        res["respstatus"]["rmsg"] = str(e) + " >>>>>>>>>>> contact support"
         return res
-    # print(body)
-    # return res
-    elif signature.fid == "F-AU001":
-        res["respstatus"]["rcode"] = "000"
-        res["respstatus"]["rmsg"] = "valid call"
-        return res
+
+        
 
